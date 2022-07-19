7bc34d990999d19608ce4ddc26934f7b3021287f
Jacob Stephens <jacob.stephens.701@gmail.com>
2022-07-19 00:44:32 -0400

    link to jacobstephens.net

--- public/index.php
+++ public/index.php
@@ -25,7 +25,7 @@ require_login();
       <li class="main-menu"><a href="<?php echo url_for('/reset/index.php'); ?>">Reset password</a></li>      
     </ul>
 
-    <p>You can use this site to generate a list of use-by dates for objects. <a href="https://jacobcstephens.com" target="_blank">Jacob Stephens</a> uses this tool to track usage of their books, ensuring they use each book either in the next or previous 180 days. <a href="https://www.theminimalists.com/ninety/" target="_blank">The Minimalists' 90/90 Rule</a> inspired Jacob to create this tool.</p>
+    <p>You can use this site to generate a list of use-by dates for objects. <a href="https://jacobstephens.net" target="_blank">Jacob Stephens</a> uses this tool to track usage of their books, ensuring they use each book either in the next or previous 180 days. <a href="https://www.theminimalists.com/ninety/" target="_blank">The Minimalists' 90/90 Rule</a> inspired Jacob to create this tool.</p>
 
   </div>
 
