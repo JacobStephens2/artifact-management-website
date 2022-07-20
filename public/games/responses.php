ccb6da1d3691af522605ef5e0aeef789f4829318
Jacob Stephens <jacob.stephens.701@gmail.com>
2022-07-19 23:08:31 -0400

    add classes to <td>'s in responses

--- public/games/responses.php
+++ public/games/responses.php
@@ -1,14 +1,10 @@
-<?php require_once('../../artifacts_private/initialize.php'); ?>
-
-<?php
+<?php 
+require_once('../../artifacts_private/initialize.php');
 require_login();
-
-  $use_set = find_responses_by_user_id();
-
+$use_set = find_responses_by_user_id();
+$page_title = 'Game responses';
+include(SHARED_PATH . '/staff_header.php');
 ?>
-
-<?php $page_title = 'Game responses'; ?>
-<?php include(SHARED_PATH . '/staff_header.php'); ?>
 
 <div id="content">
   <a class="back-link" href="<?php echo url_for('/games/index.php'); ?>">&laquo; Games</a>
@@ -17,33 +13,35 @@ require_login();
 
       <li><a class="action" href="<?php echo url_for('/games/response-new.php'); ?>">New response</a></li>
       <li><a class="action" href="<?php echo url_for('/games/playby.php'); ?>">Play games by date</a></li>
-      <br>
 
   	<table class="list">
   	  <tr>
         <th>Play date</th>
         <th>Title</th>
         <th>Player</th>
-  	    <th>&nbsp;</th>
-  	    <th>&nbsp;</th>
-        <th>&nbsp;</th>
   	  </tr>
 
       <?php while($use = mysqli_fetch_assoc($use_set)) { ?>
         <tr>
-          <td><?php echo h($use['PlayDate']); ?></td>
-    	    <td><?php echo h($use['Title']); ?></td>
-    	    <td><?php echo h($use['FirstName']) . ' ' . h($use['LastName']); ?></td>
-          <td><a class="action" href="<?php echo url_for('/games/response-show.php?id=' . h(u($use['id']))); ?>">View</a></td>
-          <td><a class="action" href="<?php echo url_for('/games/response-edit.php?id=' . h(u($use['id']))); ?>">Edit</a></td>
-          <td><a class="action" href="<?php echo url_for('/games/response-delete.php?id=' . h(u($use['id']))); ?>">Delete</a></td>
+
+          <td class="date">
+            <a class="action" href="<?php echo url_for('/games/response-edit.php?id=' . h(u($use['id']))); ?>">
+              <?php echo h($use['PlayDate']); ?>
+            </a>
+          </td>
+
+    	    <td>
+            <?php echo h($use['Title']); ?>
+          </td>
+    	    
+          <td class="playerName">
+            <?php echo h($use['FirstName']) . ' ' . h($use['LastName']); ?>
+          </td>
     	  </tr>
       <?php } ?>
   	</table>
 
-    <?php
-      mysqli_free_result($use_set);
-    ?>
+    <?php mysqli_free_result($use_set); ?>
   </div>
 
 </div>
