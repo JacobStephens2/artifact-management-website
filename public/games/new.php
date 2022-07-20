7e6aefe5bb554cc566228786901ab84672e31cd7
Jacob Stephens <jacob.stephens.701@gmail.com>
2022-07-19 23:07:37 -0400

    replace dl, dt, and dd with label

--- public/games/new.php
+++ public/games/new.php
@@ -41,38 +41,32 @@ if(is_post_request()) {
   <a class="back-link" href="<?php echo url_for('/games/index.php'); ?>">&laquo; Back to List</a>
 
   <div class="object new">
-    <h1>Create game</h1>
+    <h1>Create Artifact</h1>
 
     <?php echo display_errors($errors); ?>
 
     <form action="<?php echo url_for('/games/new.php'); ?>" method="post">
-      <dl>
-        <dt>Title</dt>
-        <dd><input type="text" name="Title" value="<?php echo h($object['Title']); ?>" /></dd>
-      </dl>
-      <dl>
-        <dt>type</dt>
-        <dd>
-          <select name="type">
-            <option value="board-game" selected>Board Game</option>
-            <option value="role-playing-game">Role Playing Game</option>
-            <option value="video-game">Video Game</option>
-            <option value="sport">Sport</option>
-            <option value="game">Game</option>
-          </select>
-        </dd>
-      </dl>
-      <dl>
-        <dt>Acq</dt>
-        <dd><input type="date" name="Acq" value="<?php echo date('Y') . '-' . date('m') . '-' . date('d'); ?>"/></dd>
-      </dl>
-      <dl>
-        <dt>KeptCol</dt>
-        <dd>
-          <input type="hidden" name="KeptCol" value="0" />
-          <input type="checkbox" name="KeptCol" value="1" checked/>
-        </dd>
-      </dl>
+
+      <label for="Title">Name</label>
+      <input type="text" name="Title" id="Title" value="<?php echo h($object['Title']); ?>" /></dd>
+      
+      <label for="type">Type</label>
+      <select name="type" id="type">
+        <option value="board-game" selected>Board Game</option>
+        <option value="role-playing-game">Role Playing Game</option>
+        <option value="video-game">Video Game</option>
+        <option value="sport">Sport</option>
+        <option value="game">Game</option>
+        <option value="video">Video</option>
+      </select>
+      
+      <label for="Acq">Acquisition Date</label>
+      <input type="date" name="Acq" id="Acq" value="<?php echo date('Y') . '-' . date('m') . '-' . date('d'); ?>"/>
+
+      <label for="KeptCol">Kept in Collection (Checked Means Yes)</label>
+      <input type="hidden" name="KeptCol" value="0" />
+      <input type="checkbox" name="KeptCol" value="1" checked/>
+
       <div id="operations">
         <input type="submit" value="Create game" />
       </div>
