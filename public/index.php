76054296313cc999c967cb8495f42743941d1b98
Jacob Stephens <jacob.stephens.701@gmail.com>
2022-07-21 01:02:24 -0400

    move artifacts section to top of main menu

--- public/index.php
+++ public/index.php
@@ -10,17 +10,19 @@ require_login();
   <div id="main-menu">
     <h2>Main Menu</h2>
     <ul>
-      <li class="main-menu"><a href="<?php echo url_for('/objects/index.php'); ?>">Objects</a></li>
-      <li class="main-menu list-2"><a href="<?php echo url_for('/object_uses/new.php'); ?>">Record use</a></li>
-      <li class="main-menu list-2"><a href="<?php echo url_for('/object_uses/index.php'); ?>">Uses</a></li>
-      <li class="main-menu list-2"><a href="<?php echo url_for('/objects/useby.php'); ?>">Use artifacts by date list</a></li>
-      <li class="main-menu"><a href="<?php echo url_for('/games/index.php');?>">Games</a></li>
-      <li class="main-menu list-2"><a href="<?php echo url_for('/players/index.php');?>">Players</a></li>
-      <li class="main-menu list-2"><a href="<?php echo url_for('/games/playgroup.php');?>">Playgroup</a></li>
-      <li class="main-menu list-2"><a href="<?php echo url_for('/games/responses.php');?>">Responses</a></li>
-      <li class="main-menu list-2"><a href="<?php echo url_for('/games/response-new.php');?>">New response</a></li>
+      <li class="main-menu"><a href="<?php echo url_for('/games/index.php');?>">Artifacts</a></li>
+      <li class="main-menu list-2"><a href="<?php echo url_for('/players/index.php');?>">Users</a></li>
+      <li class="main-menu list-2"><a href="<?php echo url_for('/games/responses.php');?>">Uses</a></li>
+      <li class="main-menu list-2"><a href="<?php echo url_for('/games/response-new.php');?>">New Use</a></li>
       <li class="main-menu list-2"><a href=" <?php echo url_for('/games/playby.php');?>">Use Artifacts by Date List</a></li>
-      <li class="main-menu list-2"><a href=" <?php echo url_for('/explore/index.php');?>">Explore games by characteristic</a></li>
+      <li class="main-menu list-2"><a href="<?php echo url_for('/games/playgroup.php');?>">Group</a></li>
+      <li class="main-menu list-2"><a href=" <?php echo url_for('/explore/index.php');?>">Explore Artifacts by Characteristic</a></li>
+
+      <li class="main-menu"><a href="<?php echo url_for('/objects/index.php'); ?>">Objects</a></li>
+      <li class="main-menu list-2"><a href="<?php echo url_for('/object_uses/new.php'); ?>">Record Use</a></li>
+      <li class="main-menu list-2"><a href="<?php echo url_for('/object_uses/index.php'); ?>">Uses</a></li>
+      <li class="main-menu list-2"><a href="<?php echo url_for('/objects/useby.php'); ?>">Use Artifacts by Date List</a></li>
+
       <li class="main-menu"><a href="<?php echo url_for('/reset/index.php'); ?>">Reset password</a></li>
     </ul>
 
