cccf570d4dc0507b1fb3ae81c9900b8140e05a69
Jacob Stephens <jacob.stephens.701@gmail.com>
2022-07-21 01:02:47 -0400

    expand labels for show artifact

--- public/games/show.php
+++ public/games/show.php
@@ -1,47 +1,34 @@
-<?php require_once('../../artifacts_private/initialize.php'); ?>
-
-<?php require_login();
-
-// $id = isset($_GET['id']) ? $_GET['id'] : '1';
-$id = $_GET['id'] ?? '1'; // PHP > 7.0
-
+<?php 
+require_once('../../artifacts_private/initialize.php');
+require_login();
+$id = $_GET['id'] ?? '1';
 $object = find_game_by_id($id);
-
+$page_title = 'Show object';
+include(SHARED_PATH . '/staff_header.php');
 ?>
 
-<?php $page_title = 'Show object'; ?>
-<?php include(SHARED_PATH . '/staff_header.php'); ?>
-
-<div id="content">
+<main>
 
   <li><a class="back-link" href="<?php echo url_for('/games/index.php'); ?>">&laquo; To game list</a></li>
   <li><a class="back-link" href="<?php echo url_for('/games/playby.php'); ?>">&laquo; To play by list</a></li>
-  <li><a class="action" href="<?php echo url_for('/games/edit.php?id=' . h(u($object['id']))); ?>">Edit</a></li>
+  
+  <h1>Title: <?php echo h($object['Title']); ?></h1>
+  
+  <dl>
+    <dt>Acquisition Date</dt>
+    <dd><?php echo h($object['Acq']); ?></dd>
+  </dl>
+  
+  <dl>
+    <dt>Kept in Collection?</dt>
+    <dd><?php echo $object['KeptCol'] == '1' ? 'true' : 'false'; ?></dd>
+  </dl>
+  
+  <dl>
+    <dt>Type</dt>
+    <dd><?php echo h($object['type']); ?></dd>
+  </dl>
 
-  <div class="object show">
-
-    <h1>Title: <?php echo h($object['Title']); ?></h1>
-
-    <div class="attributes">
-      <dl>
-        <dt>Acq</dt>
-        <dd><?php echo h($object['Acq']); ?></dd>
-      </dl>
-      <dl>
-        <dt>KeptCol</dt>
-        <dd><?php echo $object['KeptCol'] == '1' ? 'true' : 'false'; ?></dd>
-      </dl>
-      <dl>
-        <dt>type</dt>
-        <dd><?php echo h($object['type']); ?></dd>
-      </dl>
-    </div>
-
-    <hr />
-
-  </div>
-
-
-  </div>
-
-</div>
+  <li><a class="back-link" href="<?php echo url_for('/games/edit.php?id=' . h(u($object['id']))); ?>">Edit</a></li>
+  
+</main>
