f61c47982eb684fadfebe7ced419024a46abe1d5
Jacob Stephens <jacob.stephens.701@gmail.com>
2022-07-23 00:02:19 -0400

    change response language on delete page to use

--- public/games/response-delete.php
+++ public/games/response-delete.php
@@ -28,16 +28,16 @@ if(is_post_request()) {
   <a class="back-link" href="<?php echo url_for('/games/responses.php'); ?>">&laquo; Back to List</a>
 
   <div class="response-delete">
-    <h1>Delete playgroup</h1>
-    <p>Are you sure you want to delete this response?</p>
-    <p class="item">Response id: <?php echo h($use['id']); ?></p>
+    <h1>Delete Use</h1>
+    <p>Are you sure you want to delete this use?</p>
+    <p class="item">use id: <?php echo h($use['id']); ?></p>
     <p class="item">Play date: <?php echo h($use['PlayDate']); ?></p>
     <p class="item">Game: <?php echo h($use['Title']); ?></p>
     <p class="item">Player: <?php echo h($use['FirstName']) . ' ' . h($use['LastName']); ?></p>
 
     <form action="<?php echo url_for('/games/response-delete.php?id=' . h(u($use['id']))); ?>" method="post">
       <div id="operations">
-        <input type="submit" name="commit" value="Delete response" />
+        <input type="submit" name="commit" value="Delete Use" />
       </div>
     </form>
   </div>
