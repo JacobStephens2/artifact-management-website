98760ca08c8e41e5921ea2b43ba5fc79048c1d53
Jacob Stephens <jacob.stephens.701@gmail.com>
2022-07-23 00:44:02 -0400

    archive object menu

--- public/index.php
+++ public/index.php
@@ -18,12 +18,13 @@ require_login();
       <li class="main-menu list-2"><a href="<?php echo url_for('/games/playgroup.php');?>">Group</a></li>
       <li class="main-menu list-2"><a href=" <?php echo url_for('/explore/index.php');?>">Explore Artifacts by Characteristic</a></li>
 
+      <li class="main-menu"><a href="<?php echo url_for('/reset/index.php'); ?>">Reset password</a></li>
+
+      <h2>Archived Menu</h2>
       <li class="main-menu"><a href="<?php echo url_for('/objects/index.php'); ?>">Objects</a></li>
       <li class="main-menu list-2"><a href="<?php echo url_for('/object_uses/new.php'); ?>">Record Use</a></li>
       <li class="main-menu list-2"><a href="<?php echo url_for('/object_uses/index.php'); ?>">Uses</a></li>
       <li class="main-menu list-2"><a href="<?php echo url_for('/objects/useby.php'); ?>">Use Artifacts by Date List</a></li>
-
-      <li class="main-menu"><a href="<?php echo url_for('/reset/index.php'); ?>">Reset password</a></li>
     </ul>
 
     <p>You can use this site to generate a list of use-by dates for objects. <a href="https://jacobstephens.net" target="_blank">Jacob Stephens</a> uses this tool to track usage of their books, ensuring they use each book either in the next or previous 180 days. <a href="https://www.theminimalists.com/ninety/" target="_blank">The Minimalists' 90/90 Rule</a> inspired Jacob to create this tool.</p>
