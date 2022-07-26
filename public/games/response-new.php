2a5113278074b62e483c2ba7a802386f92d04842
Jacob Stephens <jacob.stephens.701@gmail.com>
2022-07-25 23:14:05 -0400

    add create artifact and create user links on response new page

--- public/games/response-new.php
+++ public/games/response-new.php
@@ -54,6 +54,12 @@ if(is_post_request()) {
   <li>
 		<a class="back-link" href="<?php echo url_for('/games/responses.php'); ?>">&laquo; Uses</a>
 	</li>
+  <li>
+		<a class="back-link" href="<?php echo url_for('/games/new.php'); ?>">&laquo; Create Artifact</a>
+	</li>
+  <li>
+		<a class="back-link" href="<?php echo url_for('/players/new.php'); ?>">&laquo; Create User</a>
+	</li>
 
     <h1>Record Use</h1>
 
