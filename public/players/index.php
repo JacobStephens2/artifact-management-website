070f0d6dbd7f7fec2973e43db3c5aa575e8f873f
Jacob Stephens <jacob.stephens.701@gmail.com>
2022-07-30 01:40:45 -0400

    move edit link onto user name

--- public/players/index.php
+++ public/players/index.php
@@ -1,15 +1,12 @@
-<?php require_once('../../artifacts_private/initialize.php'); ?>
-
-<?php
+<?php 
+require_once('../../artifacts_private/initialize.php');
 require_login();
-
-  $plural_page_name = 'Users';
-  $singular_page_name = 'User';
-  $player_set = find_players_by_user_id();
+$plural_page_name = 'Users';
+$singular_page_name = 'User';
+$player_set = find_players_by_user_id();
+$page_title = 'objects';
+include(SHARED_PATH . '/header.php');
 ?>
-
-<?php $page_title = 'objects'; ?>
-<?php include(SHARED_PATH . '/header.php'); ?>
 
 <main>
   <div class="objects listing">
@@ -20,23 +17,23 @@ require_login();
     </div>
 
   	<table class="list">
-  	  <tr>
+  	  <tr id="headerRow">
         <!-- <th>ID</th> -->
         <th>Name&ensp;</th>
         <th>Gender&ensp;</th>
   	    <th>Age&ensp;</th>
-  	    <th>&nbsp;</th>
-  	    <th>&nbsp;</th>
         <th>&nbsp;</th>
   	  </tr>
 
       <?php while($player = mysqli_fetch_assoc($player_set)) { ?>
         <tr>
-          <td><?php echo h($player['FirstName']) . ' ' . h($player['LastName']); ?></td>
+          <td>
+            <a class="table-action" href="<?php echo url_for('/players/edit.php?id=' . h(u($player['id']))); ?>">
+              <?php echo h($player['FirstName']) . ' ' . h($player['LastName']); ?>
+            </a>
+          </td>
     	    <td><?php echo h($player['G']); ?></td>
     	    <td><?php echo h($player['Age']); ?></td>
-          <td><a class="table-action" href="<?php echo url_for('/players/show.php?id=' . h(u($player['id']))); ?>">View</a></td>
-          <td><a class="table-action" href="<?php echo url_for('/players/edit.php?id=' . h(u($player['id']))); ?>">Edit</a></td>
           <td><a class="table-action" href="<?php echo url_for('/players/delete.php?id=' . h(u($player['id']))); ?>">Delete</a></td>
     	  </tr>
       <?php } ?>
