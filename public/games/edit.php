5ed9c164d6523ed33c289ba7690845b05df98ec6
Jacob Stephens <jacob.stephens.701@gmail.com>
2022-07-30 01:33:35 -0400

    remove duplication in option selection display

--- public/games/edit.php
+++ public/games/edit.php
@@ -59,58 +59,19 @@ if(is_post_request()) {
         <dt>Type</dt>
         <dd>
           <?php $type = $game['type']; ?>
-          <?php if($type == 'board-game') {
-            ?>
-            <select name="type">
-              <option value="board-game" selected>Board Game</option>
-              <option value="role-playing-game">Role Playing Game</option>
-              <option value="video-game">Video Game</option>
-              <option value="sport">Sport</option>
-              <option value="game">Game</option>
-            </select>
-            <?php
-          } elseif ($type == 'role-playing-game') {
-            ?>
-            <select name="type">
-              <option value="board-game">Board Game</option>
-              <option value="role-playing-game" selected>Role Playing Game</option>
-              <option value="video-game">Video Game</option>
-              <option value="sport">Sport</option>
-              <option value="game">Game</option>
-            </select>
-            <?php
-          } elseif ($type == 'video-game') {
-            ?>
-            <select name="type">
-              <option value="board-game">Board Game</option>
-              <option value="role-playing-game">Role Playing Game</option>
-              <option value="video-game" selected>Video Game</option>
-              <option value="sport">Sport</option>
-              <option value="game">Game</option>
-            </select>
-            <?php
-          } elseif ($type == 'sport') {
-            ?>
-            <select name="type">
-              <option value="board-game">Board Game</option>
-              <option value="role-playing-game">Role Playing Game</option>
-              <option value="video-game">Video Game</option>
-              <option value="sport" selected>Sport</option>
-              <option value="game">Game</option>
-            </select>
-            <?php
-          } else {
-            ?>
-            <select name="type">
-              <option value="board-game">Board Game</option>
-              <option value="role-playing-game">Role Playing Game</option>
-              <option value="video-game">Video Game</option>
-              <option value="sport">Sport</option>
-              <option value="game" selected>Game</option>
-            </select>
-            <?php
-          }
-          ?>
+          
+          <select name="type">
+            <option value="board-game" <?php if($type == 'board-game') { echo 'selected'; } ?>>Board Game</option>
+            <option value="role-playing-game" <?php if($type == 'role-playing-game') { echo 'selected'; } ?>>Role Playing Game</option>
+            <option value="video-game" <?php if($type == 'video-game') { echo 'selected'; } ?>>Video Game</option>
+            <option value="sport" <?php if($type == 'sport') { echo 'selected'; } ?>>Sport</option>
+            <option value="game" <?php if($type == 'game') { echo 'selected'; } ?>>Game</option>
+            <option value="vr-game" <?php if($type == 'vr-game') { echo 'selected'; } ?>>VR Game</option>
+            <option value="video" <?php if($type == 'video') { echo 'selected'; } ?>>Video</option>
+            <option value="toy" <?php if($type == 'toy') { echo 'selected'; } ?>>Toy</option>
+            <option value="mobile-game" <?php if($type == 'mobile-game') { echo 'selected'; } ?>>Toy</option>
+          </select>
+
         </dd>
       <dl>
         <dt>Acquisition Date</dt>
