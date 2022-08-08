d5d8bb49de2b81c00f9b539eb95dd34b0a9c6b1d
Jacob Stephens <jacob.stephens.701@gmail.com>
2022-08-08 08:44:46 -0400

    break about component out of pages to shared file

--- public/games/about-useby.php
+++ public/games/about-useby.php
@@ -4,13 +4,8 @@
 <?php include(SHARED_PATH . '/header.php'); ?>
 
 <main>
-    <p><a class="back-link" href="<?php echo url_for('/objects/useby.php'); ?>">&laquo; Back to Use By list</a></p>
-  <div class="objects listing" style="padding-top: 0;">
-    <h1>Details about use by dates</h1>
-    <p>The use by date for determined by the two following rules.</p>
-    <ul>
-      <li class="bullet">If the object has not yet been used, the use by date is 180 days from the acquisition date.</li>
-      <li class="bullet">Otherwise, the use by date is 360 days from the most recent use date.</li>
-    </ul>
-    <p>The list is limited to the 100 nearest use by date objects. Keeping these rules enables kept objects to be used either within the past 180 days or the coming 180 days. This approach is probably best used as a guideline rather than as a strict rule. <a href="https://www.theminimalists.com/ninety/" target='_blank'>The Minimalists' 90/90 Rule</a> inspired Jacob, operator of Steward Goods, to use these rules to track their possessions.</p>
+  <p><a class="back-link" href="<?php echo url_for('/objects/useby.php'); ?>">&laquo; Back to Use By list</a></p>
+  <?php include(SHARED_PATH . '/about.html'); ?>
+</main>
 
+<?php include(SHARED_PATH . '/footer.php'); ?>
