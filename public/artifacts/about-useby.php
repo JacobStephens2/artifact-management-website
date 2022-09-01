bc3bd0af730b10793affbf1f4dc3fa073862743e
Jacob Stephens <jacob.stephens.701@gmail.com>
2022-09-01 18:02:25 -0400

    remove extra php tags

--- public/artifacts/about-useby.php
+++ public/artifacts/about-useby.php
@@ -1,10 +1,16 @@
-<?php require_once('../../artifacts_private/initialize.php'); ?>
-<?php require_login(); ?>
-<?php $page_title = 'About Use By'; ?>
-<?php include(SHARED_PATH . '/header.php'); ?>
+<?php 
+require_once('../../artifacts_private/initialize.php');
+require_login();
+$page_title = 'About Use By';
+include(SHARED_PATH . '/header.php');
+?>
 
 <main>
-  <p><a class="back-link" href="<?php echo url_for('/artifacts/useby.php'); ?>">&laquo; Back to Use Artifacts By Date</a></p>
+  <p>
+    <a class="back-link" href="<?php echo url_for('/artifacts/useby.php'); ?>">
+      &laquo; Back to Use Artifacts By Date
+    </a>
+  </p>
   <?php include(SHARED_PATH . '/about.html'); ?>
 </main>
 
