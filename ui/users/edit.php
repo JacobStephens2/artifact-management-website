49276b9979622300545bf62f604774b05393eaca
Jacob Stephens <jacob.stephens.701@gmail.com>
2022-10-04 00:11:34 -0400

    add list of user uses

--- ui/users/edit.php
+++ ui/users/edit.php
@@ -35,41 +35,86 @@ if(is_post_request()) {
 
 ?>
 
-<?php $page_title = 'Edit player'; ?>
+<?php $page_title = 'Edit User'; ?>
 <?php include(SHARED_PATH . '/header.php'); ?>
 
 <main>
 
-  <li><a class="back-link" href="<?php echo url_for('/users/index.php'); ?>">&laquo; Players</a></li>
-
   <div class="object edit">
-    <h1>Edit player</h1>
+    <h1><?php echo $page_title; ?></h1>
 
     <?php echo display_errors($errors); ?>
 
     <form action="<?php echo url_for('/users/edit.php?id=' . h(u($id))); ?>" method="post">
-      <dl>
-        <dt>First Name</dt>
-        <dd><input type="text" name="FirstName" value="<?php echo h($player['FirstName']); ?>" /></dd>
-      </dl>
-      <dl>
-        <dt>Last Name</dt>
-        <dd><input type="text" name="LastName" value="<?php echo h($player['LastName']); ?>" /></dd>
-      </dl>
-      <dl>
-        <dt>Gender (M, F, or Other)</dt>
-        <dd><input type="text" name="G" value="<?php echo h($player['G']); ?>" /></dd>
-      </dl>
-      <dl>
-        <dt>Age</dt>
-        <dd><input type="text" name="Age" value="<?php echo h($player['Age']); ?>" /></dd>
-      </dl>
-      <div id="operations">
-        <input type="submit" value="Save player edits" />
-      </div>
+
+      <label for="FirstName">First Name</label>
+      <input 
+        type="text" 
+        name="FirstName" 
+        id="FirstName"
+        value="<?php echo h($player['FirstName']); ?>" 
+      />
+
+      <label for="LastName">Last Name</label>
+      <input 
+        type="text" 
+        id="LastName"
+        name="LastName" 
+        value="<?php echo h($player['LastName']); ?>" 
+      />
+
+      <label for="Gender">Gender (M, F, or Other)</label>
+      <input type="text" id="Gender" name="G" value="<?php echo h($player['G']); ?>" />
+
+      <label for="Age">Age</dt>
+      <input type="text" id="Age" name="Age" value="<?php echo h($player['Age']); ?>" />
+
+      <input type="submit" value="Save Edits" />
+
     </form>
 
   </div>
+
+  <section>
+    <?php
+    $sql = "SELECT 
+      responses.PlayDate, 
+      responses.id as responseID,
+      games.Title,
+      games.id AS artifactID
+      FROM responses 
+      JOIN games ON games.id = responses.Title
+      WHERE responses.Player = '" . $_REQUEST['id'] . "' 
+      ORDER BY responses.PlayDate DESC
+    ";
+    $resultObject = mysqli_query($db, $sql);
+    ?>
+    <h2>
+      <?php echo $resultObject->num_rows; ?>
+      <?php echo h($player['FullName']); ?>
+      uses are recorded 
+    </h2>
+    <table>
+      <tr>
+        <th>Use Date (<?php echo $resultObject->num_rows; ?>)</th>
+        <th>Artifact</th>
+      <tr>
+      <?php foreach ($resultObject as $resultArray) { ?>        
+        <tr>
+          <td>
+            <a href="/uses/edit.php?id=<?php echo $resultArray['responseID']; ?>">
+              <?php echo $resultArray['PlayDate']; ?>
+            </a>
+          </td>
+          <td>
+            <a href="/artifacts/edit.php?id=<?php echo $resultArray['artifactID']; ?>">
+              <?php echo $resultArray['Title']; ?>
+            </a>
+          </td>
+        </tr>
+      <?php } ?>
+    </table>
+  </section>
 
 </main>
 
