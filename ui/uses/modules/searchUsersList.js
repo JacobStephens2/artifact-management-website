ca425ac079581d623db472b8eb9fde2bf803ee49
Jacob Stephens <jacob.stephens.701@gmail.com>
2023-01-10 22:08:24 -0500

    add user dropdown search to dev use page

---
+++ ui/uses/modules/searchUsersList.js
@@ -0,0 +1,54 @@
+import { API_ORIGIN } from "./publicEnvironmentVariables.js";
+
+// Search artifacts
+
+function searchUsers(e) {
+  let requestBody = {
+    query: e.target.value,
+  };
+  fetch("https://" + API_ORIGIN + "/users.php", {
+    method: "POST",
+    credentials: "include",
+    body: JSON.stringify(requestBody),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.authenticated == false) {
+        location.href = "/login.php";
+      } else {
+        const userResultsList = document.querySelector('ul.userResults');
+        userResultsList.innerHTML = '';
+        if (data.users.length > 10) {
+          var userSearchResultsListLength = 10;
+        } else {
+          var userSearchResultsListLength = data.users.length;
+        }
+        for (let i = 0; i < userSearchResultsListLength; i++) {
+          let listItem = document.createElement("li");
+          listItem.value = data.users[i].id;
+          listItem.innerText = data.users[i].FullName;
+          listItem.addEventListener('click', function() {
+            document.querySelector('input#user0id').value = data.users[i].id;
+            document.querySelector('input#user0name').value = data.users[i].FullName;
+            hideSearchResults();
+          });
+          userResultsList.append(listItem);
+        }
+      }
+    });
+}
+
+const searchUsersInput = document.querySelector("input.user");
+searchUsersInput.addEventListener("input", searchUsers);
+
+// Show and hide search results
+
+function showSearchResults() {
+  document.querySelector('div.userResults').style.display = 'block';
+}
+
+function hideSearchResults() {
+  document.querySelector('div.userResults').style.display = 'none';
+}
+
+searchUsersInput.addEventListener('focus', showSearchResults);
--- ui/uses/new.php
+++ ui/uses/new.php
@@ -1,39 +1,96 @@
 <?php
 require_once('../../private/initialize.php');
 require_login();
+
+if(is_post_request()) {
+
+  ?>
+  <?php
+
+  $response = [];
+  $response['Title'] = $_POST['Title'] ?? '';
+  $response['PlayDate'] = $_POST['PlayDate'] ?? '';
+  $response['Note'] = $_POST['Note'] ?? '';
+
+  $response['Player1'] = $_POST['Player1'] ?? '';
+  $response['Player2'] = $_POST['Player2'] ?? '';
+  $response['Player3'] = $_POST['Player3'] ?? '';
+  $response['Player4'] = $_POST['Player4'] ?? '';
+  $response['Player5'] = $_POST['Player5'] ?? '';
+  $response['Player6'] = $_POST['Player6'] ?? '';
+  $response['Player7'] = $_POST['Player7'] ?? '';
+  $response['Player8'] = $_POST['Player8'] ?? '';
+  $response['Player9'] = $_POST['Player9'] ?? '';
+
+  $playerCount = $_GET['playerCount'] ?? 1;
+
+  // $result = insert_response($response, $playerCount);
+
+  // if($result === true) {
+  //   $new_id = mysqli_insert_id($db);
+  //   $_SESSION['message'] = "The response was recorded successfully.";
+  //   redirect_to(url_for('/uses/create.php'));
+  // } else {
+  //   $errors = $result;
+  // }
+
+} else {
+  // display the blank form
+  $response = [];
+  $response["Title"] = '';
+  $response["PlayDate"] = '';
+  $response["Player"] = '';
+  $playerCount = $_GET['playerCount'] ?? 1;
+}
+
 $page_title = 'Record Use';
 include(SHARED_PATH . '/header.php'); 
 ?>
 
-<script type="module" src="modules/getArtifacts.js"></script>
 <script type="module" src="modules/searchArtifactsList.js"></script>
+<script type="module" src="modules/searchUsersList.js"></script>
 <script type="module" src="modules/getUsers.js"></script>
 
 <main>
+
+  <?php
+    if (is_post_request()) {
+      ?>
+      <pre>$_POST: <?php print_r($_POST); ?></pre>
+      <?php
+    }
+  ?>
 
   <h1>
     <?php echo $page_title; ?>
   </h1>
 
-  <form action="new-use.php" method="post">
+  <form action="new.php" method="post">
     
     <label for="date">Date</label>
     <input type="date" name="use[date]" id="date" 
       value="<?php echo date('Y-m-d'); ?>"  
     >
 
-    <label for="Title">Artifact</label>
-    <select name="Title" id="Title">
-    </select>
-
     <label for="SearchTitles">Search Artifacts</label>
-    <input type="search" id="SearchTitles" name="searchTitle" value="test">
+    <input type="search" id="SearchTitles" name="searchTitle" value="">
+    <input type="hidden" id="SearchTitleSubmission" name="searchTitleID" value="">
     <div class="searchResults" style="display: none;">
       <ul class="searchResults" style="margin-top: 0;">
-        <li id="result0" value="1">One</li>
-        <li id="result1" value="2">Two</li>
+        <li></li>
       </ul>
     </div>
+
+    <label for="users">Users List</label>
+    <section id="users">
+      <input type="search" class="user" id="user0name" name="user[][name]" value="">
+      <input type="hidden" id="user0id" name="user[][id]" value="">
+      <div class="userResults" style="display: none;">
+        <ul class="userResults" style="margin-top: 0;">
+          <li></li>
+        </ul>
+      </div>
+    </section>
 
     <label for="Users">Users</label>
     <section id="sweetSpots">
