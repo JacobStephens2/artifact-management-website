d16c5ee12b7141d11f17aefaeb3bd6ebbbb0e625
Jacob Stephens <jacob.stephens.701@gmail.com>
2023-01-11 22:43:07 -0500

    insert responses on form submission

--- ui/uses/new.php
+++ ui/uses/new.php
@@ -4,43 +4,52 @@ require_login();
 
 if(is_post_request()) {
 
-  ?>
-  <?php
+  /*
+    Sample post request body
 
-  $response = [];
-  $response['Title'] = $_POST['Title'] ?? '';
-  $response['PlayDate'] = $_POST['PlayDate'] ?? '';
-  $response['Note'] = $_POST['Note'] ?? '';
+    $_POST: Array 
+    (
+      [useDate] => 2023-01-12
+      [artifact] => Array
+        (
+            [name] => Age of Empires IV
+            [id] => 2807
+        )
 
-  $response['Player1'] = $_POST['Player1'] ?? '';
-  $response['Player2'] = $_POST['Player2'] ?? '';
-  $response['Player3'] = $_POST['Player3'] ?? '';
-  $response['Player4'] = $_POST['Player4'] ?? '';
-  $response['Player5'] = $_POST['Player5'] ?? '';
-  $response['Player6'] = $_POST['Player6'] ?? '';
-  $response['Player7'] = $_POST['Player7'] ?? '';
-  $response['Player8'] = $_POST['Player8'] ?? '';
-  $response['Player9'] = $_POST['Player9'] ?? '';
+      [user] => Array
+        (
+          [0] => Array
+              (
+                  [name] => Jacob Stephens
+                  [id] => 141
+              )
 
-  $playerCount = $_GET['playerCount'] ?? 1;
+          [1] => Array
+              (
+                  [name] => Luke Boerman
+                  [id] => 91
+              )
 
-  // $result = insert_response($response, $playerCount);
+          [2] => Array
+              (
+                  [name] => Jarren Horrocks
+                  [id] => 26
+              )
+        )
+    )
+  */
 
-  // if($result === true) {
-  //   $new_id = mysqli_insert_id($db);
-  //   $_SESSION['message'] = "The response was recorded successfully.";
-  //   redirect_to(url_for('/uses/create.php'));
-  // } else {
-  //   $errors = $result;
-  // }
 
-} else {
-  // display the blank form
-  $response = [];
-  $response["Title"] = '';
-  $response["PlayDate"] = '';
-  $response["Player"] = '';
-  $playerCount = $_GET['playerCount'] ?? 1;
+  $insertResult = insert_response_revised($_POST);
+
+  if($insertResult === true) {
+    $new_id = mysqli_insert_id($db);
+    $_SESSION['message'] = "The response was recorded successfully.";
+    redirect_to(url_for('/uses/new.php'));
+  } else {
+    $errors = $insertResult;
+  }
+
 }
 
 $page_title = 'Record Use';
@@ -53,14 +62,6 @@ include(SHARED_PATH . '/header.php');
 
 <main>
 
-  <?php
-    if (is_post_request()) {
-      ?>
-      <pre>$_POST: <?php print_r($_POST); ?></pre>
-      <?php
-    }
-  ?>
-
   <h1>
     <?php echo $page_title; ?>
   </h1>
@@ -68,13 +69,13 @@ include(SHARED_PATH . '/header.php');
   <form action="new.php" method="post">
     
     <label for="date">Date</label>
-    <input type="date" name="use[date]" id="date" 
+    <input type="date" name="useDate" id="date" 
       value="<?php echo date('Y-m-d'); ?>"  
     >
 
     <label for="SearchTitles">Search Artifacts</label>
-    <input type="search" id="SearchTitles" name="searchTitle" value="">
-    <input type="hidden" id="SearchTitleSubmission" name="searchTitleID" value="">
+    <input type="search" id="SearchTitles" name="artifact[name]" value="">
+    <input type="hidden" id="SearchTitleSubmission" name="artifact[id]" value="">
     <div class="searchResults" style="display: none;">
       <ul class="searchResults" style="margin-top: 0;">
         <li></li>
@@ -83,25 +84,16 @@ include(SHARED_PATH . '/header.php');
 
     <label for="users">Users List</label>
     <section id="users">
-      <input type="search" class="user" id="user0name" name="user[][name]" value="">
-      <input type="hidden" id="user0id" name="user[][id]" value="">
+      <input type="search" class="user" id="user0name" name="user[0][name]" 
+        value="<?php echo $_SESSION['FullName']; ?>"
+      >
+      <input type="hidden" id="user0id" name="user[0][id]" 
+        value="<?php echo $_SESSION['player_id']; ?>"
+      >
       <div class="userResults" style="display: none;">
         <ul class="userResults" style="margin-top: 0;">
           <li></li>
         </ul>
-      </div>
-    </section>
-
-    <label for="Users">Users</label>
-    <section id="sweetSpots">
-      <div>
-        <input type="hidden" name="User[][id]" value="<?php echo $_SESSION['player_id']; ?>">
-        <input 
-          class="sweetSpot"
-          type="text" 
-          id="User" 
-          value="<?php echo $_SESSION['FullName']; ?>"
-        >
       </div>
     </section>
 
