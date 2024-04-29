0ec552dcecd44680974f84713e0fad6ac99d0cc0
Jacob Stephens <jacob.stephens.701@gmail.com>
2024-04-28 20:29:30 -0400

    get book to be default type

--- private/shared/artifact_type_options.php
+++ private/shared/artifact_type_options.php
@@ -2,12 +2,19 @@
 
   require_once 'artifact_type_array.php';
   global $typesArray;
+  var_dump($type);
 
   foreach ($typesArray as $typeCategory) {
     ?>
     <option 
       value="<?php echo $typeCategory; ?>" 
-      <?php if ($type == $typeCategory ) { echo 'selected' ; } ?>
+      <?php
+        if ($type === '' && $typeCategory === 'book') {
+          echo 'selected';
+        } elseif ($type == $typeCategory ) { 
+          echo 'selected' ; 
+        } 
+      ?>
       >
       <?php echo $typeCategory; ?>
     </option>
