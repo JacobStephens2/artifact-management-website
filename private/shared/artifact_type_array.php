84d38dda102b0b086ad4d0cadd82d48f4b9d3b7f
Jacob Stephens <jacob.stephens.701@gmail.com>
2024-07-30 22:51:13 -0400

    give access to cron to all types in types table

--- private/shared/artifact_type_array.php
+++ private/shared/artifact_type_array.php
@@ -1,12 +1,23 @@
 <?php   // define types array
-  $user_id = $_SESSION['user_id'];
-  
-  $result = query(
-    "SELECT id, ObjectType AS type
-    FROM types
-    WHERE user_id = '$user_id'
-    ORDER BY type ASC
-  ");
+  if (isset($_SESSION['user_id'])) {
+    $user_id = $_SESSION['user_id'];
+    
+    $result = query(
+      "SELECT id, ObjectType AS type
+      FROM types
+      WHERE user_id = '$user_id'
+      ORDER BY type ASC
+    ");
+
+  } else {
+
+    $result = query(
+      "SELECT id, ObjectType AS type
+      FROM types
+      ORDER BY type ASC
+    ");
+    
+  }
 
   foreach ($result as $row) {
     $typesArray[$row['type']] = $row['id'];
